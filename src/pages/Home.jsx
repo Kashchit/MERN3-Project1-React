ee5fdb680055acd2e51dcd2710e04cce115b7f43
Kashchit <kashchit10@gmail.com>
2025-09-21 00:30:26 +0530

    cors

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,33 @@
 import axios from 'axios'
+import { useState } from 'react'
 import { useEffect } from 'react'
 import Card from '../components/Card'
 import Navbar from '../components/Navbar'
 
 function Home(){
+    const [blogs, setBlogs] = useState([])
     const fetchBlogs = async()=>{
         const response = await axios.get("http://localhost:3000/blog")
+        setBlogs(response.data.data)
     }
     useEffect(()=>{
         fetchBlogs()
     },[])
+    console.log(blogs)
 
     return(
         <>
     <Navbar />
       <div className='flex flex-wrap'>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
+        {
+            blogs.map(function(blog){       //mapping the array
+                return(
+                    <Card blog = {blog}/>
+                )
+            })
+        }
+      
+      
       </div>
         <h1>Home page</h1>
         </>
