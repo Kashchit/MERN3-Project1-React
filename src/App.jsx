c002edfdfea133f84fa1e2c8a40ecb1497308aac
Kashchit <kashchit10@gmail.com>
2025-09-20 23:26:20 +0530

    connecting to the backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Card from './components/Card'
-import Navbar from './components/Navbar'
 import Home from './pages/Home'
 
 function App() {
   return (
     <>
-      <Navbar />
-      <div className='flex flex-wrap'>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      </div>
+      
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,28 @@
-
+import axios from 'axios'
+import { useEffect } from 'react'
+import Card from '../components/Card'
+import Navbar from '../components/Navbar'
 
 function Home(){
+    const fetchBlogs = async()=>{
+        const response = await axios.get("http://localhost:3000/blog")
+    }
+    useEffect(()=>{
+        fetchBlogs()
+    },[])
+
     return(
         <>
+    <Navbar />
+      <div className='flex flex-wrap'>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      </div>
         <h1>Home page</h1>
         </>
     )
